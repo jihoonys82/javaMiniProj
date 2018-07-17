2d72c42df5cc665d237a6b408165da3f425fcc56
user2 <user2@KH_B>
2018-07-17 21:28:47 +0900

    DetailDialog

---
+++ InJung/src/injung/DetailDialog.java
@@ -0,0 +1,20 @@
+package injung;
+
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+public class DetailDialog extends JDialog {
+
+	private static final long serialVersionUID = 5858592773353644590L;
+
+	public DetailDialog( ) {
+		setBounds(700, 500, 100,100);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);	
+	}
+	
+	public DetailDialog(EmployeeInfoPanel pane) {
+		this();
+		this.add(pane);
+		pane.setVisible(true);
+	}
+}
