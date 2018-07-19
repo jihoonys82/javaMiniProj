ed51994fefa00d7be7205765d865c16aaf7e3cfa
JuHyeon <JuHyeon@pc1>
2018-07-19 15:14:39 +0900

    JUHYEON

--- InJung/src/injung/LoginPanel.java
+++ InJung/src/injung/LoginPanel.java
@@ -44,8 +44,10 @@ class LoginPanel extends JDialog implements ActionListener, KeyListener {
 	public static final int LOGIN_SUCCESSED = 0;	// 로그인 성공일 때 
 	public static final int LOGIN_FAILED = 1;		// 로그인 실패일 때 
 	public static final int LOGIN_NO_ID = 2;		// DB에 없는 ID값일 때
+	public static final int NO_NUMBER = 3;
 
 	public LoginPanel(JFrame frame, String title, boolean modal) {
+		
 		super(frame, title, true);
 		setTitle("Login");
 		setLocation(550, 250);
@@ -62,9 +64,14 @@ class LoginPanel extends JDialog implements ActionListener, KeyListener {
 		lblPw = new JLabel("패스워드 : "); // 패스워드 레이블 생성
 		lblPw.setBounds(46, 90, 77, 21);
 		
+//		NumberFormatter formatter = new NumberFormatter();
+//		formatter.
+//		formatter.setAllowsInvalid(false);
+		
 		txtEmployeeId = new JTextField(); // 사번 텍스트 생성
 		txtEmployeeId.setBounds(152, 53, 154, 21);
-
+		txtEmployeeId.addKeyListener(this);
+		
 		txtPw = new JPasswordField(); // 패스워드 텍스트 생성
 		txtPw.setBounds(152, 90, 154, 21);
 		txtPw.setEditable(true);
@@ -94,13 +101,22 @@ class LoginPanel extends JDialog implements ActionListener, KeyListener {
 	
 	@Override
 	public void keyPressed(KeyEvent e) { // 엔터를 누를 때 로그인 체크 메소드 실행 
+//		char ch = txtEmployeeId.getText().charAt(0);
+//		char[] ch_1 = txtEmployeeId.getText().toCharArray();
+//		char ch_2 = e.getKeyChar();
+		
 		if (e.getKeyCode() == KeyEvent.VK_ENTER) {		
-			btnLogin.doClick();
+			btnLogin.doClick(); 
 		}
+		
+//		} else if (!(Character.isDigit(ch_2))) {
+//			JOptionPane.showMessageDialog(null, "문자임");
+//		}
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) { }
+	
 	@Override
 	public void keyReleased(KeyEvent e) { }
 
@@ -112,6 +128,7 @@ class LoginPanel extends JDialog implements ActionListener, KeyListener {
 		if(loginCheck == LoginPanel.LOGIN_NO_ID) {	// DB에 사번 값이 없으면 경고 메세지 출력  
 			JOptionPane.showMessageDialog(null, "없는 정보입니다");
 		}
+		
 	}
 
 	@Override
@@ -134,20 +151,39 @@ class LoginPanel extends JDialog implements ActionListener, KeyListener {
 	}
 	
 	public int isLoginCheck(int empId) {	
+		
+//		for (char c : strTxtId.toCharArray()) {
+//			if (!Character.isDigit(c)) {
+//				System.out.println(c);
+//				JOptionPane.showMessageDialog(null, "사번을 입력해주세요");
+//				break;
+//			}
+//			System.out.print(c);
+//		}
+
 		eDto = dao.getEmployee(empId);
-		if(eDto==null) {	// 1. DTO에 없는 값이면 LOGIN_NO_ID -> 경고 메세지 출력 
+		if(eDto==null) {	// 1. DB에 없는 값이면 LOGIN_NO_ID -> 경고 메세지 출력 
 			return LOGIN_NO_ID;
 		}
-		int login_result = LOGIN_FAILED;	
+		int login_result = LOGIN_FAILED;			
+		
+		String strTxtId = new String(txtEmployeeId.getText());	// ID 텍스트필드에서 가져온 값 담기
+		char chTxtId = strTxtId.charAt(0);
+		if (!Character.isDigit(chTxtId)) {
+			return NO_NUMBER;
+		}
+		
+		int id = eDto.getEmployeeId();
+		int it = Integer.parseInt(txtEmployeeId.getText());
 		
 		char[] chTxtPw = txtPw.getPassword();	
 		String strTxtPw = new String(chTxtPw);	// 패스워드필드에서 가져온 값 String에 담기
-		String strTxtId = new String(txtEmployeeId.getText());	// ID 텍스트필드에서 가져온 값 담기 	
 		String strDbPw = new String(eDto.getPassword());	// DTO에서 가져온 패스워드값 String에 담기  
 		int intId = new Integer(eDto.getEmployeeId());		
 		String strDbId = Integer.toString(intId);			// DTO에서 가져온 ID값 담기 
 		
-		if ((strTxtId.equals(strDbId))
+			
+		if ((id == it)
 				&& (strTxtPw.equals(strDbPw))) { // 2. 입력 사번과 DB사번이 같고, 입력 패스워드와 DB 패스워드가 같다면						
 			login_result = LOGIN_SUCCESSED; // 로그인 성공
 			dispose(); // 로그인창 종료
@@ -155,6 +191,11 @@ class LoginPanel extends JDialog implements ActionListener, KeyListener {
 		} else if  (!(strTxtId.equals(strDbId))
 				|| (!(strTxtPw.equals(strDbPw))) )  { 		// 3. 입력 사번과 DB사번이 다르거나, 입력 패스워드와 DB 패스워드가 다르다면					
 			lblWarn.setText("<html> 로그인 정보가 일치하지 않습니다 <br/> 다시 입력해주세요 </html>"); // 경고레이블 출력
+			
+			char ch_0 = strTxtId.charAt(0);
+			if (!(Character.isDigit(ch_0))) {
+				JOptionPane.showMessageDialog(null,"X");
+			}
 			login_result = LOGIN_FAILED;
 		} 
 		return login_result; // 로그인 결과 리턴		
